57d1b194a94b1bc56a8dc8d750dc28cfd14eb967
Theodor Li <theodor2311@gmail.com>
2020-09-10 23:58:39 +0800

    Version 2

--- openshift-test.go
+++ openshift-test.go
@@ -13,5 +13,5 @@ func main() {
 
 // OpenShiftTest Handler
 func OpenShiftTest(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "<h1>OpenShift Test Version 1</h1><p>Pod Name: %s <br>Namespace: %s<br>IP Address: %s<br>Node Name: %s<br>Service Account: %s</p>", os.Getenv("POD_NAME"), os.Getenv("POD_NAMESPACE"), os.Getenv("POD_IP"), os.Getenv("NODE_NAME"), os.Getenv("POD_SERVICE_ACCOUNT"))
+	fmt.Fprintf(res, "<h1>OpenShift Test Version 2</h1><p>Pod Name: %s <br>Namespace: %s<br>IP Address: %s<br>Node Name: %s<br>Service Account: %s</p>", os.Getenv("POD_NAME"), os.Getenv("POD_NAMESPACE"), os.Getenv("POD_IP"), os.Getenv("NODE_NAME"), os.Getenv("POD_SERVICE_ACCOUNT"))
 }
